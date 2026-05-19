c4ac9bde38e8f397c5b222314239c8af4856bc18
Brendan Chen <me@bchen.dev>
2026-05-18 18:06:44 -0700

    Initial Commit

---
+++ Kaifang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kaifang
+//
+//  Created by Brendan Chen on 2026.05.18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KaifangTests/KaifangTests.swift
@@ -0,0 +1,19 @@
+//
+//  KaifangTests.swift
+//  KaifangTests
+//
+//  Created by Brendan Chen on 2026.05.18.
+//
+
+import Testing
+@testable import Kaifang
+
+struct KaifangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
