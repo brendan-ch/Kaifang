2257d46748b08b251bcca87f9511dd30cf85ebc9
Brendan Chen <me@bchen.dev>
2026-05-26 09:50:23 -0700

    Adjust PersistenceController to use the CloudKit container
    - Add containerIdentifier option to map to a CloudKit container; if nil, then CloudKit sync is off

--- KaifangShared/Sources/KaifangCore/PersistenceController.swift
+++ KaifangShared/Sources/KaifangCore/PersistenceController.swift
@@ -10,15 +10,35 @@ import CoreData
 public final class PersistenceController {
     public let container: NSPersistentContainer
     
-    public init(inMemory: Bool = false) {
+    public init(inMemory: Bool = false, containerIdentifier: String? = nil) {
         let modelURL = Bundle.module.url(forResource: "KaifangModel", withExtension: "momd")!
         let model = NSManagedObjectModel(contentsOf: modelURL)!
-        container = NSPersistentContainer(name: "KaifangModel", managedObjectModel: model)
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        container = NSPersistentCloudKitContainer(name: "KaifangModel", managedObjectModel: model)
+        
+        guard let description = container.persistentStoreDescriptions.first else {
+            fatalError("No persistent store description")
         }
+        description.setOption(true as NSNumber, forKey: NSPersistentHistoryTokenKey)
+        description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+        
+        // note: relaunch needed if this setting is changed
+        // also need a separate path to delete iCloud data if toggled off
+        if let containerIdentifier = containerIdentifier {
+            description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(
+                containerIdentifier: containerIdentifier
+            )
+        } else {
+            description.cloudKitContainerOptions = nil
+        }
+        
+        if inMemory {
+            description.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
         container.loadPersistentStores { _, error in
             if let error { fatalError("Core Data failed: \(error)") }
         }
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
     }
 }
