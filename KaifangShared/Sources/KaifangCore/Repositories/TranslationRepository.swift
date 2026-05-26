9b220247d8f80871fc04479654293c6fb81c4d06
Brendan Chen <me@bchen.dev>
2026-05-25 17:51:20 -0700

    Define TranslationRepositoryTests test cases

---
+++ KaifangShared/Sources/KaifangCore/Repositories/TranslationRepository.swift
@@ -0,0 +1,18 @@
+//
+//  TranslationRepository.swift
+//  KaifangShared
+//
+//  Created by Brendan Chen on 2026.05.25.
+//
+
+import CoreData
+
+public class TranslationRepository {
+    private let context: NSManagedObjectContext
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+    }
+    
+    
+}
---
+++ KaifangShared/Tests/KaifangCoreTests/RepositoryTests/TranslationRepositoryTests.swift
@@ -0,0 +1,46 @@
+//
+//  TranslationRepositoryTests.swift
+//  KaifangShared
+//
+//  Created by Brendan Chen on 2026.05.25.
+//
+
+import Testing
+@testable import KaifangCore
+
+struct TranslationRepositoryTests {
+    @Test("Translation lookup returns the relevant translation by original text, original language and translation language if it exists")
+    func translationLookupGetsCorrectTranslation() async throws {
+        
+    }
+    
+    @Test("Translation lookup returns nil if the translation doesn't exist")
+    func translationLookupReturnsNilIfNoTranslation() async throws {
+        
+    }
+    
+    @Test("Translation lookup by original text is case-insensitive")
+    func translationLookupIsCaseInsensitive() async throws {
+        
+    }
+    
+    @Test("Saving a new translation saves the context")
+    func saveTranslationPersistsTranslationInContext() async throws {
+        
+    }
+    
+    @Test("Saving a translation with the same original text + language overwrites the old translation")
+    func saveExistingTranslationWithOriginalTextOverwritesOld() async throws {
+        
+    }
+    
+    @Test("Deleting a translation via ID removes it from the context immediately")
+    func deleteTranslationDeletesFromContext() async throws {
+        
+    }
+    
+    @Test("Clearing all translations clears the stored translations")
+    func clearAllTranslationsClearsStoredTranslationsFromContext() async throws {
+        
+    }
+}
