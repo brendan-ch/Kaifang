097dbe7ab266533ffaf03420a34f1e4057cc0d7f
Brendan Chen <me@bchen.dev>
2026-05-27 10:01:44 -0700

    Update CDTranslationModel with properties
    Includes a JSON binary data blob for storing metadata (excluding API key)

---
+++ KaifangShared/Sources/KaifangCore/EntityDeclarations/CDTranslationModel.swift
@@ -0,0 +1,12 @@
+//
+//  CDTranslationProvider.swift
+//  KaifangShared
+//
+//  Created by Brendan Chen on 2026.05.26.
+//
+
+import CoreData
+
+@objc(CDTranslationModel)
+public class CDTranslationModel: CDEntityWithIDAndDateMetadata {
+}
