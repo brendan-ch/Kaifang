51d2bd737a7e2d0a45873e88c3623b63dd2989b5
Brendan Chen <me@bchen.dev>
2026-05-29 16:47:10 -0700

    Add stub ArticleProvider.Article

---
+++ KaifangShared/Sources/KaifangCore/Providers/ArticleProvider+DataTypes.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleProvider+DataTypes.swift
+//  KaifangShared
+//
+//  Created by Brendan Chen on 2026.05.29.
+//
+
+import Foundation
+
+public extension ArticleProvider {
+    /// Maps to ``CDBaseArticle``.
+    struct Article: Equatable, Sendable {
+        let id: UUID
+        
+        // stub implementation, add other properties later
+    }
+}
